2c33a9deb50f81bf0497ed29a98292ccd139e51b
Hooman Foroudastan <hforoudastan@lulu.com>
2022-01-16 09:05:23 -0500

    questions(07, 08): scope, equals vs strict equals

---
+++ 07_scopeMyCar.js
@@ -0,0 +1,31 @@
+// Question: Take a look at the following code...
+// What is logged to the console when the method "myCar.logColor()" is ran?
+var myCar = {
+  color: "Blue",
+  logColor: function () {
+    // The "this" keyword references the object it is inside of.
+    // "this" is referencing the "myCar" object.
+    // Setting the variable "self" equal to "this" is common practice in JavaScript
+    // Its purpose is to store a reference to a certain scope or context that you will need to use later on.
+    var self = this;
+    console.log("In logColor - this.color: " + this.color); // logs Blue
+    console.log("In logColor - self.color: " + self.color); // logs Blue
+    (function () {
+      // Here, we are inside of the scope of the unnamed, IIFE function.
+      // Since the IIFE function is NOT a method of the "myCar" object ...
+      // ... our "this" keyword has lost reference to the "myCar" object.
+      // Instead, it is now referencing the global object.
+      // In many cases; if you use the "this" keyword inside of nested functions ...
+      // ... you are more than likely going to lose reference to the object you are inside of.
+      // Resulting in your "this" keyword referencing the global object.
+      console.log("In IIFE - this.color: " + this.color); // logs undefined
+      console.log("In IIFE - self.color: " + self.color); // logs Blue
+    })();
+  },
+};
+
+myCar.logColor(); // logs the following...
+// In logColor - this.color: Blue
+// In logColor - self.color: Blue
+// In IIFE - this.color: undefined
+// In IIFE - self.color: Blue
---
+++ 08_equalsVsStrictEquals.js
@@ -0,0 +1,33 @@
+// Question: What is the difference between the comparison operators "==" and "==="?
+// In other words; what is the difference between the comparison operators ...
+// ... double equals "==" and triple equals "==="?
+// Answer: "==" is known as "equals" and it tests for abstract equality.
+// "===" is known as "strict equals" and it tests for strict equality.
+
+// In more simple terms; the double equals (i.e. "=="), does NOT test for data type.
+// The triple equals (i.e. "==="), DOES test for data type.
+
+// For example:
+// Does 7 as a number equal to "7" as a string?
+// Since we are comparing two different types of data;
+// One being a number and the other being a string ...
+// The double equals (i.e. "==") will convert both pieces of data to the same type ...
+// ... and then compare them.
+console.log(7 == "7"); // logs true
+
+// Changing the comparison operator to a triple equals logs "false".
+// "false" is logged because the triple equals is "strict" and it DOES test for the type of each piece of data.
+console.log(7 === "7"); // logs false
+
+// Question: What happens "under the hood" when using == to compare compare pieces of data?
+// Answer: If we are comparing a "number" & "string":
+// 1. "string" is converted to a "number"
+// 2. comparison is made
+
+// If we are comparing a "boolean" & "non-boolean":
+// 1. "non-boolean" is converted to a "boolean"
+// 2. comparison is made
+
+// If we are comparing an "object" & "primitive data-type" (i.e. "number" or "string"):
+// 1. "object" is converted to a "primitive data-type" (i.e. "number" or "string")
+// 2. comparison is made
